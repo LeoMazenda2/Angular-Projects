fba6d1a20600e759a31c3de96c6fcec4c308cc02
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-01-09 16:10:42 +0100

    fazendo a ultoma alua do reactive form

--- projeto1 - modulo 3/src/app/features/produto/cadastro/cadastro.component.ts
+++ projeto1 - modulo 3/src/app/features/produto/cadastro/cadastro.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../services/produto.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from '../models/produto.model';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -17,7 +17,7 @@ export class CadastroComponent implements OnInit {
   isNovoProduto: boolean = false;
   tituloPagina: string = '';
 
-  formCadastroProduto!: FormGroup:
+  formCadastroProduto!: FormGroup;
 
   constructor(private produtoService: ProdutoService,
     private activatedeRoute: ActivatedRoute,
@@ -37,10 +37,10 @@ export class CadastroComponent implements OnInit {
       this.produtoService.getprodutoPeloId(this.id).subscribe((produto: Produto) => {
         //template driven (depressiado)
         this.produto = produto;
-        this.formCadastroProduto.controls.['nome'].setValue(this.produto.nome);
-        this.formCadastroProduto.controls.('descicao').setValue(this.produto.descricao);
-        this.formCadastroProduto.controls.('preco').setValue(this.produto.preco);
-        this.formCadastroProduto.controls.('estoque').setValue(this.produto.estoque);
+        this.formCadastroProduto.controls['nome'].setValue(this.produto.nome);
+        this.formCadastroProduto.controls['descicao'].setValue(this.produto.descricao);
+        this.formCadastroProduto.controls['estoque'].setValue(this.produto.estoque);
+        this.formCadastroProduto.controls['preco'].setValue(this.produto.preco);
         this.tituloPagina = `A editar producto`
       });
     } else {
@@ -52,28 +52,59 @@ export class CadastroComponent implements OnInit {
 
  criarFormularrio () {
   this.formCadastroProduto = this.formBuilder.group({
-    nome: '',
-    descicao: '',
-    preco: '',
-    estoque: 0
+    nome: ['', Validators.required],
+    descricao: ['', Validators.required],
+    email: ['', [Validators.required, Validators.pattern("^[a-z0-9.%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],//validação de email
+    preco: ['', Validators.required],
+    estoque: [0, Validators.required]
   });
  }
 
 
   salvarProduto() {
-    // const produtoParaSalavar: Produto = {
-    //   id: parseInt(this.id),
-    // }
+    const produtoParaSalavar: Produto = {
+      id: parseInt(this.id),
+      nome: this.formCadastroProduto.controls['nome'].value,
+      descricao: this.formCadastroProduto.controls['descricao'].value,
+      estoque: this.formCadastroProduto.controls['estoque'].value,
+      preco: this.formCadastroProduto.controls['preco'].value,
+    }
 
-    // console.log(produtoParaSalavar);
+    console.log(this.formCadastroProduto.value);
 
-    // if (this.isNovoProduto) {
-    //   this.adiconarProduto(produtoParaSalavar);
-    // } else {
-    //   produtoParaSalavar.imagemUrl = this.produto.imagemUrl;
-    //   this.atualizarProduto(produtoParaSalavar);
-    // }
+    if (this.isNovoProduto) {
+      this.adiconarProduto(produtoParaSalavar);
+    } else {
+      produtoParaSalavar.imagemUrl = this.produto.imagemUrl;
+      this.atualizarProduto(produtoParaSalavar);
+    }
   }
+
+  
+//  salvarProduto() {
+//   let produtos: Produto[] = JSON.parse(localStorage.getItem('produtos') || '[]');
+//   let novoId = produtos.length > 0 ? Math.max(...produtos.map(p => p.id)) + 1 : 1;
+
+//   const produtoParaSalavar: Produto = {
+//     id: this.isNovoProduto ? novoId : parseInt(this.id),
+//     nome: this.formCadastroProduto.value.nome,
+//     descricao: this.formCadastroProduto.value.descricao,
+//     estoque: this.formCadastroProduto.value.estoque,
+//     preco: this.formCadastroProduto.value.preco,
+//   };
+
+//   console.log(this.formCadastroProduto.value);
+
+//   if (this.isNovoProduto) {
+//     this.adiconarProduto(produtoParaSalavar);
+//     produtos.push(produtoParaSalavar);
+//     localStorage.setItem('produtos', JSON.stringify(produtos));
+//   } else {
+//     produtoParaSalavar.imagemUrl = this.produto.imagemUrl;
+//     this.atualizarProduto(produtoParaSalavar);
+//   }
+// }
+
 
   atualizarProduto(produtoParaSalavar: Produto) {
     this.produtoService.actualizarProduto(produtoParaSalavar).subscribe(Response => {
