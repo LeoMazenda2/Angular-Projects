4b9aa9589dbf68e0bfb363f36f669569822e56ce
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-01-09 11:18:28 +0100

    Aplicano reactive form

--- projeto1 - modulo 3/src/app/features/produto/cadastro/cadastro.component.ts
+++ projeto1 - modulo 3/src/app/features/produto/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../services/produto.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from '../models/produto.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,75 +13,77 @@ export class CadastroComponent implements OnInit {
 
   id!: string;
   produto!: Produto
-  rota: string ='';
+  rota: string = '';
   isNovoProduto: boolean = false;
+  tituloPagina: string = '';
 
-  //template driven (depressiado)
-  nome: string = '';
-  descricao: string = '';
-  preco: string = '';
-  estoque: number = 0;
-  
-  tituloPagina: string ='';
+  formCadastroProduto!: FormGroup:
 
-  constructor(private produtoService: ProdutoService, 
+  constructor(private produtoService: ProdutoService,
     private activatedeRoute: ActivatedRoute,
-    private router: Router) {
+    private router: Router,
+    private formBuilder: FormBuilder) {
 
-     }
+  }
 
   ngOnInit(): void {
-   // Recuperar o Producto pelo ID
-   this.rota = this.activatedeRoute.snapshot.url[0].path;
+    // Recuperar o Producto pelo ID
+    this.rota = this.activatedeRoute.snapshot.url[0].path;
+    this.criarFormularrio();
 
-    if(this.rota === 'editar-produto') {
+    if (this.rota === 'editar-produto') {
       this.id = this.activatedeRoute.snapshot.url[1].path;
-   
+
       this.produtoService.getprodutoPeloId(this.id).subscribe((produto: Produto) => {
         //template driven (depressiado)
-         this.produto = produto;
-         this.nome = produto.nome;
-         this.descricao = produto.descricao;
-         this.preco = produto.preco;
-         this.estoque = produto.estoque;  
-         this.tituloPagina = `A editar producto`    
+        this.produto = produto;
+        this.formCadastroProduto.controls.['nome'].setValue(this.produto.nome);
+        this.formCadastroProduto.controls.('descicao').setValue(this.produto.descricao);
+        this.formCadastroProduto.controls.('preco').setValue(this.produto.preco);
+        this.formCadastroProduto.controls.('estoque').setValue(this.produto.estoque);
+        this.tituloPagina = `A editar producto`
       });
     } else {
       this.isNovoProduto = true;
-      this.tituloPagina = 'A adiconar Producto'
+      this.tituloPagina = 'A adiconar Producto';
+
     }
-
-
   }
 
-  salvarProduto(){
-    const produtoParaSalavar: Produto = {
-      id: parseInt(this.id),
-      nome: this.nome,
-      preco: this.preco,    
-      descricao: this.descricao,
-      estoque: this.estoque
-    }
+ criarFormularrio () {
+  this.formCadastroProduto = this.formBuilder.group({
+    nome: '',
+    descicao: '',
+    preco: '',
+    estoque: 0
+  });
+ }
 
-    console.log(produtoParaSalavar);
 
-    if(this.isNovoProduto){
-      this.adiconarProduto(produtoParaSalavar);
-    } else{
-      produtoParaSalavar.imagemUrl = this.produto.imagemUrl;
-      this.atualizarProduto(produtoParaSalavar);
-    }    
+  salvarProduto() {
+    // const produtoParaSalavar: Produto = {
+    //   id: parseInt(this.id),
+    // }
+
+    // console.log(produtoParaSalavar);
+
+    // if (this.isNovoProduto) {
+    //   this.adiconarProduto(produtoParaSalavar);
+    // } else {
+    //   produtoParaSalavar.imagemUrl = this.produto.imagemUrl;
+    //   this.atualizarProduto(produtoParaSalavar);
+    // }
   }
 
-  atualizarProduto(produtoParaSalavar: Produto){
-    this.produtoService.actualizarProduto(produtoParaSalavar).subscribe(Response =>{
-      this.router.navigate(["produto","listagem"]);
+  atualizarProduto(produtoParaSalavar: Produto) {
+    this.produtoService.actualizarProduto(produtoParaSalavar).subscribe(Response => {
+      this.router.navigate(["produto", "listagem"]);
     });
   }
 
-  adiconarProduto(produtoParaSalavar: Produto){
-    this.produtoService.aicionarProduto(produtoParaSalavar).subscribe(Response =>{
-      this.router.navigate(["produto","listagem"]);
+  adiconarProduto(produtoParaSalavar: Produto) {
+    this.produtoService.aicionarProduto(produtoParaSalavar).subscribe(Response => {
+      this.router.navigate(["produto", "listagem"]);
     });
   }
 }
